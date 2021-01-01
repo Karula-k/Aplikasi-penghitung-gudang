654b917ffecb82cc54b845d337f8623cec19faf6
Karula-k <king.waterf@gmail.com>
2021-01-01 19:20:35 +0700

    asistensi

--- sql.py
+++ sql.py
@@ -1,22 +1,95 @@
 import sqlite3
-class Data_source:
+import abc
+class barang(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def addItems(self):
+        pass
+    @abc.abstractmethod
+    def viewData(self):
+        pass
+    @abc.abstractmethod
+    def searchData(self):
+        pass
+    @abc.abstractmethod
+    def DataUpdate(self):
+        pass
+class liquid(barang):
+    def add_items(self,name,Supplier,harga,tanggal_masuk,rak,type_benda,expire_liquid):
+        con=sqlite3.connect(self.db_name)
+        cur =con.cursor()
+        cur.execute("insert into "+self.table_name+" values (NULL,?,?,?,?,?,?,?)",
+        (name,Supplier,harga,tanggal_masuk,rak,type_benda,expire_liquid))
+        con.commit()
+        con.close()
+    def Search_Data(self,name="",Supplier="",harga="",tanggal_masuk="",rak="",type_benda="",expire_liquid=""):
+        con=sqlite3.connect(self.db_name)
+        cur =con.cursor()
+        text ="Select * From "+self.table_name+" where name=? or Supplier=? or harga=? or tanggal_masuk=? \
+            or rak =? or type_benda=? or expire_liquid=?"
+        cur.execute(text,(name,Supplier,harga,tanggal_masuk,rak,type_benda,expire_liquid))
+        rows =cur.fetchall()
+        con.close()
+        return rows
+
+    def Data_Update (self,name=None,Supplier=None,harga=None,tanggal_masuk=None,rak=None,expire_liquid=None,idbr=-1):
+        con=sqlite3.connect(self.db_name)
+        cur =con.cursor()
+        dict_items = {"name":name,"Supplier":Supplier,"harga":harga,"tanggal_masuk":tanggal_masuk,"rak":rak,"\
+            type_benda":"liquid","expire_liquid":expire_liquid}
+        text2 = {k: v for k, v in dict_items.items() if v is not None}
+        text ="Update "+self.table_name+" Set "+",".join([i+"="+"'"+text2[i]+"'"for i in text2])+" \
+            where id="+str(idbr)
+        cur.execute(text)
+        con.commit()
+        con.close()    
+class padatan(barang):
+    def add_items(self,name,Supplier,harga,tanggal_masuk,rak,type_benda,dimensi_benda):
+        con=sqlite3.connect(self.db_name)
+        cur =con.cursor()
+        cur.execute("insert into "+self.table_name+" values (NULL,?,?,?,?,?,?,?)",
+        (name,Supplier,harga,tanggal_masuk,rak,type_benda,dimensi_benda))
+        con.commit()
+        con.close()
+
+    def Search_Data(self,name="",Supplier="",harga="",tanggal_masuk="",rak="",type_benda="",dimnesi=""):
+        con=sqlite3.connect(self.db_name)
+        cur =con.cursor()
+        text ="Select * From "+self.table_name+" where name=? or Supplier=? or harga=? or tanggal_masuk=?or rak =? or type_benda=? or dimensi_benda=?"
+        cur.execute(text,(name,Supplier,harga,tanggal_masuk,rak,type_benda,dimnesi))
+        rows =cur.fetchall()
+        con.close()
+        return rows
+
+    def Data_Update (self,name=None,Supplier=None,harga=None,tanggal_masuk=None,rak=None,type_benda=None,dimnesi=None,idbr=-1):
+        con=sqlite3.connect(self.db_name)
+        cur =con.cursor()
+        dict_items = {"name":name,"Supplier":Supplier,"harga":harga,"tanggal_masuk":tanggal_masuk}
+        text2 = {k: v for k, v in dict_items.items() if v is not None}
+        text ="Update "+self.table_name+" Set "+",".join([i+"="+"'"+text2[i]+"'"for i in text2])+" \
+            where id="+str(idbr)
+        cur.execute(text)
+        con.commit()
+        con.close()    
+
+class Data_source():
     def __init__(self,db_name,table_name="stockGudang"):
         self.db_name =db_name+".db"
         self.table_name = table_name
         con =sqlite3.connect(self.db_name)
         cur=con.cursor()
         create_text ="create table if not exists "+self.table_name+" (id integer primary key,name text, \
-            Supplier text, harga integer,tanggal_masuk date)"
+            Supplier text, harga integer,tanggal_masuk date,rak text,type_benda text,dimensi_benda text,expire_liquid\
+                text)"
         cur.execute(create_text)
         con.commit()
         con.close()
-
-    def add_items(self,name,Supplier,harga,tanggal_masuk):
-        con=sqlite3.connect(self.db_name)
-        cur =con.cursor()
-        cur.execute("insert into "+self.table_name+" values (NULL,?,?,?,?)",(name,Supplier,harga,tanggal_masuk))
-        con.commit()
-        con.close()
+    def add_br(self,*args):
+        if args[5]=="liquid":
+            h = liquid.addItems(args[k] for k in args)
+            print(args)
+        else:
+            h = padatan.addItems(args[k] for k in args)
+            print(args)
     def viewData(self):
         con=sqlite3.connect(self.db_name)
         cur =con.cursor()
@@ -32,25 +105,6 @@ class Data_source:
         con.commit()
         con.close()
 
-    def Search_Data(self,name="",Supplier="",harga="",tanggal_masuk=""):
-        con=sqlite3.connect(self.db_name)
-        cur =con.cursor()
-        text ="Select * From "+self.table_name+" where name=? or Supplier=? or harga=? or tanggal_masuk=?"
-        cur.execute(text,(name,Supplier,harga,tanggal_masuk))
-        rows =cur.fetchall()
-        con.close()
-        return rows
-
-    def Data_Update (self,name=None,Supplier=None,harga=None,tanggal_masuk=None,idbr=-1):
-        con=sqlite3.connect(self.db_name)
-        cur =con.cursor()
-        dict_items = {"name":name,"Supplier":Supplier,"harga":harga,"tanggal_masuk":tanggal_masuk}
-        text2 = {k: v for k, v in dict_items.items() if v is not None}
-        text ="Update "+self.table_name+" Set "+",".join([i+"="+"'"+text2[i]+"'"for i in text2])+" \
-            where id="+str(idbr)
-        cur.execute(text)
-        con.commit()
-        con.close()
     def order_by(self,type_cl):
         con=sqlite3.connect(self.db_name)
         cur =con.cursor()
@@ -107,5 +161,5 @@ class login_call(Data_source):
         row = cur.fetchall()
         con.commit()
         con.close()
-        return row
+        return row 
 uy = login_call("ayam")
